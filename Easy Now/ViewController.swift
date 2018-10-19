e4cc5ce5d1209c0648b36eb0cfa795bc16c3fff9
Irina Ioana Avadanei <iavada16@student.aau.dk>
2018-10-19 11:36:43 +0200

    Initial Commit

---
+++ Easy Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Now
+//
+//  Created by Irina Ioana Avadanei on 19/10/2018.
+//  Copyright © 2018 Irina Ioana Avadanei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Easy NowTests/Easy_NowTests.swift
@@ -0,0 +1,36 @@
+//
+//  Easy_NowTests.swift
+//  Easy NowTests
+//
+//  Created by Irina Ioana Avadanei on 19/10/2018.
+//  Copyright © 2018 Irina Ioana Avadanei. All rights reserved.
+//
+
+import XCTest
+@testable import Easy_Now
+
+class Easy_NowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
